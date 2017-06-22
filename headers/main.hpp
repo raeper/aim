6110490d6ed185b72cfba9e40274020395537f28
aa.novozhilov <aa.novozhilov@tensor.ru>
2017-06-22 11:16:55 +0300

    Clean up

--- headers/main.hpp
+++ headers/main.hpp
@@ -2,8 +2,8 @@
 #include <memory>
 #include <thread>
 
-#include "reactor.hpp"
-#include "ioactor.hpp"
+#include "events_dispenser.hpp"
+#include "input_aggregator.hpp"
 
 namespace aim {
         struct Application {
