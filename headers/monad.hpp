6b1f5068b8719b00a9852c5ebda53d9b4bfc7849
aa.novozhilov <aa.novozhilov@tensor.ru>
2017-07-07 11:18:55 +0300

    Extend monad

--- headers/monad.hpp
+++ headers/monad.hpp
@@ -1,14 +1,20 @@
+#pragma once
 #include <utility>
+#include <memory>
+#include <optional>
 
 namespace aim {
 
-        template <typename T, template <typename> typename Cont>
+        template <typename T, template <typename...> typename Cont>
         struct is_valid {};
 
-        template <typename T, template <typename> typename Cont>
+        template <typename T, template <typename...> typename Cont>
+        struct make {};
+
+        template <typename T, template <typename...> typename Cont>
         struct chain;
 
-        template <typename T, template <typename> typename Cont>
+        template <typename T, template <typename...> typename Cont>
         struct use {
                 using value_type_t = T;
                 using holder_t = Cont<value_type_t>;
@@ -24,7 +30,7 @@ namespace aim {
                 constexpr auto with(Func&& func){
                         using subresult_type_t = decltype(func(std::declval<value_type_t>()));
                         if (is_valid<T, Cont>{}(value)) {
-                                return chain<subresult_type_t, Cont>{Cont<subresult_type_t>{func(*value)}};
+                                return chain<subresult_type_t, Cont>{make<subresult_type_t, Cont>{}(std::move(func(*value)))};
                         }
                         else {
                                 return chain<subresult_type_t, Cont>{};
@@ -34,44 +40,44 @@ namespace aim {
                 holder_t value;
         };
 
-        template <typename TT, template <typename> typename ContT>
+        template <typename TT, template <typename...> typename ContT>
         struct chain : use<TT, ContT> {
                 template <typename Func>
                 constexpr auto otherwise(Func&& func) {
-                        return chain<TT, ContT>{ContT<decltype(func())>{func()}};
+                        return chain<TT, ContT>{make<decltype(func()), ContT>{}(func())};
                 }
         };
 
 }
 
-// #include <optional>
+namespace aim {
+        template <typename T>
+        struct is_valid<T, std::optional> {
+                constexpr bool operator()(std::optional<T> const& value) {
+                        return bool(value);
+                }
+        };
 
-// namespace aim {
-//         template <typename T>
-//         struct is_valid<T, std::optional> {
-//                 constexpr bool operator()(std::optional<T> const& value) {
-//                         return bool(value);
-//                 }
-//         };
-// }
+        template <typename T>
+        struct make<T, std::optional> {
+                auto operator()(T&& value) {
+                        return std::optional{std::move(value)};
+                }
+        };
+}
 
-// #include <iostream>
-// #include <memory>
+namespace aim {
+        template <typename T>
+        struct is_valid<T, std::unique_ptr> {
+                constexpr bool operator()(std::unique_ptr<T> const& value) {
+                        return bool(value);
+                }
+        };
 
-// int main() {
-
-//         {
-//                 std::optional<int> a;
-
-//                 aim::use(std::move(a)).with([](auto v) {
-//                         std::cout << v << '\n';
-//                         return 1;
-//                 }).otherwise([](){
-//                         std::cout << "it's none!\n";
-//                         return 2;
-//                 }).with([](auto v){
-//                         std::cout <<  v << '\n';
-//                         return 3;
-//                 });
-//         }
-// }
+        template <typename T>
+        struct make<T, std::unique_ptr> {
+                auto operator()(T&& value) {
+                        return std::make_unique<T>(std::move(value));
+                }
+        };
+}
